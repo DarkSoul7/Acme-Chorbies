f927c65081b6e0f4eb5e9141958bf64411f3f674
rafgardom <rafgardom2@alum.us.es>
2017-04-08 22:52:01 +0200

    Corrección registro chorbi Ya se visualizan todos los errores correctamente

--- Acme-Chorbies/src/main/java/services/ChorbiService.java
+++ Acme-Chorbies/src/main/java/services/ChorbiService.java
@@ -36,182 +36,180 @@ import forms.ChorbiListForm;
 @Service
 @Transactional
 public class ChorbiService {
-	
+
 	// Managed repository
-	
+
 	@Autowired
 	private ChorbiRepository		chorbiRepository;
-	
+
 	// Supported services
 	@Autowired
 	private ActorService			actorService;
-	
+
 	@Autowired
 	private SearchTemplateService	searchTemplateService;
-	
+
 	@Autowired
 	private Validator				validator;
-	
-	
+
+
 	//Constructor
-	
+
 	public ChorbiService() {
 		super();
 	}
-	
+
 	//Simple CRUD methods
-	
+
 	public ChorbiForm createForm() {
 		ChorbiForm result;
 		UserAccount userAccount;
 		Authority authority;
-		
+
 		authority = new Authority();
 		authority.setAuthority(Authority.CHORBI);
-		
+
 		userAccount = new UserAccount();
 		userAccount.addAuthority(authority);
 		userAccount.setEnabled(true);
-		
+
 		result = new ChorbiForm();
 		result.setUserAccount(userAccount);
-		
+
 		return result;
 	}
-	
+
 	public Chorbi create() {
 		Chorbi result;
 		UserAccount userAccount;
 		Authority authority;
-		
+
 		authority = new Authority();
 		authority.setAuthority(Authority.CHORBI);
-		
+
 		userAccount = new UserAccount();
 		userAccount.addAuthority(authority);
 		userAccount.setEnabled(true);
-		
+
 		result = new Chorbi();
 		result.setUserAccount(userAccount);
-		
+
 		return result;
 	}
-	
+
 	public Collection<Chorbi> findAll() {
 		this.actorService.findByPrincipal();
 		return this.chorbiRepository.findAll();
 	}
-	
+
 	public Chorbi findOne(final int chorbiId) {
 		return this.chorbiRepository.findOne(chorbiId);
-		
+
 	}
-	
+
 	// Comprobar antes de guardar al actor que su tarjeta de crédito tenga una
 	// fecha válida (del número se encargala anotación)
 	public Chorbi save(final Chorbi chorbi) {
 		Assert.notNull(chorbi);
 		Chorbi result;
 		SearchTemplate searchTemplate;
-		
+
 		result = this.chorbiRepository.save(chorbi);
-		
+
 		if (chorbi.getId() == 0) {
 			searchTemplate = this.searchTemplateService.create(result);
 			searchTemplate.setCachedMoment(new Date(System.currentTimeMillis() - 1000));
 			searchTemplate = this.searchTemplateService.save(searchTemplate);
 			result.setSearchTemplate(searchTemplate);
 		}
-		
-		chorbiRepository.flush();
-		
+
+		this.chorbiRepository.flush();
+
 		return result;
 	}
-	
+
 	public void delete(final Chorbi chorbi) {
 		Assert.notNull(chorbi);
 		this.chorbiRepository.delete(chorbi);
 	}
-	
+
 	public void flush() {
 		this.chorbiRepository.flush();
 	}
-	
+
 	// Other business methods
-	
+
 	public Chorbi findByPrincipal() {
 		Chorbi result;
 		UserAccount userAccount;
-		
+
 		userAccount = LoginService.getPrincipal();
-		
+
 		result = this.findByUserAccount(userAccount);
 		Assert.notNull(result, "Any chorbi with userAccountId=" + userAccount.getId() + "has be found");
-		
+
 		return result;
 	}
-	
+
 	public Chorbi findByUserAccount(final UserAccount userAccount) {
 		Chorbi result;
 		int userAccountId;
-		
+
 		userAccountId = userAccount.getId();
 		result = this.chorbiRepository.findByUserAccountId(userAccountId);
-		
+
 		return result;
 	}
-	
+
 	public Chorbi findByUserName(final String username) {
 		Assert.notNull(username);
 		Chorbi result;
-		
+
 		result = this.chorbiRepository.findByUserName(username);
-		
+
 		return result;
 	}
-	
+
 	public Collection<Chorbi> findAllExceptPrincipal() {
 		Collection<Chorbi> result;
 		Chorbi principal;
-		
+
 		principal = this.findByPrincipal();
 		result = this.chorbiRepository.findAllExceptPrincipal(principal.getId());
-		
+
 		return result;
 	}
-	
-	public Chorbi reconstruct(ChorbiForm chorbiForm, BindingResult binding) throws CheckDigitException {
+
+	public Chorbi reconstruct(final ChorbiForm chorbiForm, final BindingResult binding) throws CheckDigitException {
 		Assert.notNull(chorbiForm);
 		Chorbi result;
 		String password;
-		
+
 		// Registering a new Chorbi
 		if (chorbiForm.getId() == 0) {
 			result = this.create();
-			
-			if (chorbiForm.getUserAccount().getPassword().equals(chorbiForm.getRepeatPassword())) {
+
+			if (chorbiForm.getUserAccount().getPassword().equals(chorbiForm.getRepeatPassword()) && !chorbiForm.getUserAccount().getPassword().isEmpty())
 				password = this.hashPassword(chorbiForm.getUserAccount().getPassword());
-			} else {
+			else
 				password = "";
-			}
-			
-			Collection<Like> receivedLikes = new ArrayList<>();
-			Collection<Like> authoredLikes = new ArrayList<>();
-			Collection<Chirp> sentChirps = new ArrayList<>();
-			Collection<Chirp> receivedChirps = new ArrayList<>();
-			
+
+			final Collection<Like> receivedLikes = new ArrayList<>();
+			final Collection<Like> authoredLikes = new ArrayList<>();
+			final Collection<Chirp> sentChirps = new ArrayList<>();
+			final Collection<Chirp> receivedChirps = new ArrayList<>();
+
 			result.setAuthoredLikes(authoredLikes);
 			result.setReceivedLikes(receivedLikes);
 			result.setReceivedChirps(receivedChirps);
 			result.setSentChirps(sentChirps);
 			result.setReceivedChirps(receivedChirps);
-			
+
 			result.getUserAccount().setUsername(chorbiForm.getUserAccount().getUsername());
 			result.getUserAccount().setPassword(password);
-		} else {
+		} else
 			result = this.findOne(chorbiForm.getId());
-		}
-		
+
 		// Registering or editing chorbi
 		result.setPicture(chorbiForm.getPicture());
 		result.setDescription(chorbiForm.getDescription());
@@ -220,32 +218,38 @@ public class ChorbiService {
 		result.setCoordinates(chorbiForm.getCoordinates());
 		result.setBirthDate(chorbiForm.getBirthDate());
 		result.setCreditCard(chorbiForm.getCreditCard());
-		
+
 		result.setName(chorbiForm.getName());
 		result.setSurname(chorbiForm.getSurname());
 		result.setPhone(chorbiForm.getPhone());
 		result.setEmail(chorbiForm.getEmail());
-		
+
 		// Check chorbi is over age
-		int chorbiYears = this.getChorbiAge(result);
-		if (chorbiYears < 18) {
+		final int chorbiYears = this.getChorbiAge(result);
+		if (chorbiYears < 18)
 			result.setOverAge(false);
-		} else {
+		else
 			result.setOverAge(true);
-		}
-		
+
 		// Check creditCard if any
-		if (this.analyzeCreditCard(chorbiForm.getCreditCard())) {
+		if (this.analyzeCreditCard(chorbiForm.getCreditCard()))
 			result.setValidCreditCard(this.checkCreditCard(result.getCreditCard()));
+
+		if (binding != null)
+			this.validator.validate(result, binding);
+
+		if (result.getUserAccount().getPassword() == "" || result.getUserAccount().getPassword() == null) {
+			FieldError fieldError;
+			final String[] codes = {
+				"chorbi.passwords.error"
+			};
+			fieldError = new FieldError("chorbiForm", "userAccount.password", result.getUserAccount().getPassword(), false, codes, null, "");
+			binding.addError(fieldError);
 		}
-		
-		if (binding != null) {
-			validator.validate(result, binding);
-		}
-		
+
 		return result;
 	}
-	
+
 	public ChorbiForm toFormObject(final Chorbi chorbi) {
 		Assert.notNull(chorbi);
 		final ChorbiForm result = new ChorbiForm();
@@ -261,233 +265,234 @@ public class ChorbiService {
 		result.setPicture(chorbi.getPicture());
 		result.setRelationship(chorbi.getRelationship());
 		result.setSurname(chorbi.getSurname());
-		
+
 		return result;
 	}
-	
+
 	public int getChorbiAge(final Chorbi chorbi) {
 		Assert.notNull(chorbi);
-		
+
 		final LocalDate chorbiBirthDate = new LocalDate(chorbi.getBirthDate());
 		final LocalDate now = new LocalDate();
 		final Years chorbiYears = Years.yearsBetween(chorbiBirthDate, now);
-		
+
 		return chorbiYears.getYears();
 	}
-	
+
 	private boolean analyzeCreditCard(final CreditCard creditCard) {
 		boolean result = false;
-		
-		if (creditCard != null) {
-			if (creditCard.getBrandName() != null || !creditCard.getHolderName().isEmpty() || creditCard.getCvv() != null || creditCard.getExpirationMonth() != null || creditCard.getExpirationYear() != null || !creditCard.getNumber().isEmpty()) {
+
+		if (creditCard != null)
+			if (creditCard.getBrandName() != null || !creditCard.getHolderName().isEmpty() || creditCard.getCvv() != null || creditCard.getExpirationMonth() != null || creditCard.getExpirationYear() != null || !creditCard.getNumber().isEmpty())
 				result = true;
-			}
-		}
 		return result;
 	}
-	
+
 	public boolean checkCreditCard(final CreditCard creditCard) throws CheckDigitException {
 		Assert.notNull(creditCard);
 		boolean result = false;
-		
+
 		final boolean luhn = LuhnCheckDigit.LUHN_CHECK_DIGIT.isValid(creditCard.getNumber());
 		final LocalDate localDate = new LocalDate();
 		final LocalDate expirationDate = new LocalDate(creditCard.getExpirationYear(), creditCard.getExpirationMonth(), 1);
-		
+
 		final int days = Days.daysBetween(localDate, expirationDate).getDays();
-		
-		if (luhn && (days > 1)) {
+
+		if (luhn && (days > 1))
 			result = true;
-		}
-		
+
 		return result;
 	}
-	
-	public Collection<Chorbi> findChorbisFromAuthor(int authorId) {
+
+	public Collection<Chorbi> findChorbisFromAuthor(final int authorId) {
 		Collection<Chorbi> result;
 		this.actorService.findByPrincipal();
-		
+
 		result = this.chorbiRepository.findChorbisFromAuthor(authorId);
-		
+
 		return result;
 	}
-	
-	public Collection<Chorbi> findChorbisFromReceiver(int receiverId) {
+
+	public Collection<Chorbi> findChorbisFromReceiver(final int receiverId) {
 		Collection<Chorbi> result;
 		this.actorService.findByPrincipal();
-		
+
 		result = this.chorbiRepository.findChorbisFromReceiver(receiverId);
-		
+
 		return result;
 	}
-	
-	public Collection<ChorbiListForm> findChorbisFromAuthorWithLikes(int authorId) {
+
+	public Collection<ChorbiListForm> findChorbisFromAuthorWithLikes(final int authorId) {
 		Collection<ChorbiListForm> result;
 		Actor principal;
-		
+
 		principal = this.actorService.findByPrincipal();
 		result = this.chorbiRepository.findChorbisFromAuthorWithLikes(authorId, principal.getId());
-		
+
 		return result;
 	}
-	
-	public Collection<ChorbiListForm> findChorbisFromReceiverWithLikes(int receiverId) {
+
+	public Collection<ChorbiListForm> findChorbisFromReceiverWithLikes(final int receiverId) {
 		Collection<ChorbiListForm> result;
 		Actor principal;
-		
+
 		principal = this.actorService.findByPrincipal();
 		result = this.chorbiRepository.findChorbisFromReceiverWithLikes(receiverId, principal.getId());
-		
+
 		return result;
 	}
-	
-	public Integer checkUsername(String username) {
+
+	public Integer checkUsername(final String username) {
 		Assert.isTrue(StringUtils.isNotBlank(username));
-		
+
 		return this.chorbiRepository.checkUsername(username);
 	}
-	
+
 	public Collection<ChorbiListForm> findAllExceptPrincipalWithLikes() {
 		Collection<ChorbiListForm> result;
 		Actor principal;
-		
+
 		principal = this.actorService.findByPrincipal();
 		result = this.chorbiRepository.findAllExceptPrincipalWithLikes(principal.getId());
-		
+
 		return result;
 	}
-	
-	public void checkRestrictions(ChorbiForm chorbiForm, BindingResult binding) {
+
+	public void checkRestrictions(final ChorbiForm chorbiForm, final BindingResult binding) {
 		FieldError fieldError;
-		
+
 		if (!chorbiForm.getUserAccount().getPassword().equals(chorbiForm.getRepeatPassword())) {
-			String[] codes = { "chorbi.passwords.error" };
+			final String[] codes = {
+				"chorbi.passwords.error"
+			};
 			fieldError = new FieldError("chorbiForm", "userAccount.password", chorbiForm.getUserAccount().getPassword(), false, codes, null, "");
 			binding.addError(fieldError);
 			fieldError = new FieldError("chorbiForm", "repeatPassword", chorbiForm.getUserAccount().getPassword(), false, codes, null, "");
 			binding.addError(fieldError);
 		}
-		
+
 		this.checkUniqueRestrictions(chorbiForm, binding);
 	}
-	
-	private void checkUniqueRestrictions(ChorbiForm chorbiForm, BindingResult binding) {
+
+	private void checkUniqueRestrictions(final ChorbiForm chorbiForm, final BindingResult binding) {
 		Integer usernameUnique;
 		FieldError fieldError;
-		
+
 		usernameUnique = this.chorbiRepository.checkUsername(chorbiForm.getUserAccount().getUsername());
-		
+
 		if (usernameUnique != null) {
-			String[] codes = { "chorbi.username.error" };
+			final String[] codes = {
+				"chorbi.username.error"
+			};
 			fieldError = new FieldError("chorbiForm", "userAccount.username", chorbiForm.getUserAccount().getUsername(), false, codes, null, "");
 			binding.addError(fieldError);
 		}
 	}
-	
-	private String hashPassword(String password) {
+
+	private String hashPassword(final String password) {
 		String result;
 		Md5PasswordEncoder encoder;
-		
+
 		encoder = new Md5PasswordEncoder();
 		result = encoder.encodePassword(password, null);
-		
+
 		return result;
 	}
-	
+
 	// DashBoard
-	
+
 	public Collection<Object[]> getChorbiesPerCountry() {
 		return this.chorbiRepository.getChorbiesPerCountry();
 	}
-	
+
 	public Collection<Object[]> getChorbiesPerCity() {
 		return this.chorbiRepository.getChorbiesPerCity();
 	}
-	
+
 	public Integer minChorbiesAge() {
 		return this.chorbiRepository.minChorbiesAge();
 	}
-	
+
 	public Integer maxChorbiesAge() {
 		return this.chorbiRepository.maxChorbiesAge();
 	}
-	
+
 	public Integer avgChorbiesAge() {
 		return this.chorbiRepository.avgChorbiesAge();
 	}
-	
+
 	public Double ratioChorbiesWithoutCreditCard() {
 		return this.chorbiRepository.ratioChorbiesWithoutCreditCard();
 	}
-	
+
 	public Double ratioChorbiesLookingForActivities() {
 		return this.chorbiRepository.ratioChorbiesLookingForActivities();
 	}
-	
+
 	public Double ratioChorbiesLookingForFriends() {
 		return this.chorbiRepository.ratioChorbiesLookingForFriends();
 	}
-	
+
 	public Double ratioChorbiesLookingForLove() {
 		return this.chorbiRepository.ratioChorbiesLookingForLove();
 	}
-	
+
 	public Collection<Chorbi> listChorbiesSortedByReceivedLikes() {
 		return this.chorbiRepository.listChorbiesSortedByReceivedLikes();
 	}
-	
+
 	public Integer minReceivedLikesPerChorbi() {
 		return this.chorbiRepository.minReceivedLikesPerChorbi();
 	}
-	
+
 	public Integer maxReceivedLikesPerChorbi() {
 		return this.chorbiRepository.maxReceivedLikesPerChorbi();
 	}
-	
+
 	public Double avgReceivedLikesPerChorbi() {
 		return this.chorbiRepository.avgReceivedLikesPerChorbi();
 	}
-	
+
 	public Integer minAuthoredLikesPerChorbi() {
 		return this.chorbiRepository.minAuthoredLikesPerChorbi();
 	}
-	
+
 	public Integer maxAuthoredLikesPerChorbi() {
 		return this.chorbiRepository.maxAuthoredLikesPerChorbi();
 	}
-	
+
 	public Double avgAuthoredLikesPerChorbi() {
 		return this.chorbiRepository.avgAuthoredLikesPerChorbi();
 	}
-	
+
 	public Integer minReceivedChirpsPerChorbi() {
 		return this.chorbiRepository.minReceivedChirpsPerChorbi();
 	}
-	
+
 	public Integer maxReceivedChirpsPerChorbi() {
 		return this.chorbiRepository.maxReceivedChirpsPerChorbi();
 	}
-	
+
 	public Double avgReceivedChirpsPerChorbi() {
 		return this.chorbiRepository.avgReceivedChirpsPerChorbi();
 	}
-	
+
 	public Integer minSentChirpsPerChorbi() {
 		return this.chorbiRepository.minSentChirpsPerChorbi();
 	}
-	
+
 	public Integer maxSentChirpsPerChorbi() {
 		return this.chorbiRepository.maxSentChirpsPerChorbi();
 	}
-	
+
 	public Double avgSentChirpsPerChorbi() {
 		return this.chorbiRepository.avgSentChirpsPerChorbi();
 	}
-	
+
 	public Collection<Chorbi> getChorbiMoreSentChirp() {
 		return this.chorbiRepository.getChorbiMoreSentChirp();
 	}
-	
+
 	public Collection<Chorbi> getChorbiMoreGotChirp() {
 		return this.chorbiRepository.getChorbiMoreGotChirp();
 	}
